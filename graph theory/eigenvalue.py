c06951440e48709a843122fcbece262bf9ff15a5
Rudresh Gade <rudresh.gade@students.iiserpune.ac.in>
2024-07-12 17:50:45 +0530

    1

--- graph theory/eigenvalue.py
+++ graph theory/eigenvalue.py
@@ -1,25 +1,39 @@
-# import numpy as np
-# from numpy.linalg import eig
-
-# a = np.array([[3,-1,-1,-1,0], 
-#             [-1,4,-1,-1,-1],
-#             [-1,-1,4,-1,-1],
-#             [-1,-1,-1, 4,-1],
-#             [0,-1,-1,-1,3]])
-# w,v=eig(a)
-# print('E-value:', w)
-# print('E-vector', v)
-
-import networkx
+import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-n = 4
-m = 2
-g = networkx.complete_graph(n-1)
-g.add_node(n)
-g.add_edges_from([(n,i) for i in range(m)])
+n = 8
+d = 3
+g = nx.Graph()
 
-L = networkx.laplacian_matrix(g)
+for k in range(d):
+    for i in range(k*n+1,(k+1)*n):
+        g.add_edge(i,i+1)
+    g.add_edge((k+1)*n,k*n+1)
+
+for k in range(d-1):
+    for i in range(1,n+1):
+        g.add_edge(k*n+i,(k+1)*n+i)
+for i in range(1,n+1):
+    g.add_edge(d*n+1,i)
+
+L = nx.laplacian_matrix(g)
 e = sorted(np.linalg.eigvals(L.toarray()))
+print(f"Second smallest eigenvalue : {e[1]}")
+# nx.draw_networkx(g)
+# plt.show()
 
-print(e[1])
+A = [0]
+
+for i in range(1, d * n + 2,n):
+    g_i = g.copy()
+    g_i.remove_node(i)
+    S_i = nx.laplacian_matrix(g_i)
+    E_i = sorted(np.linalg.eigvals(S_i.toarray()))
+    print(f"Second smallest eigenvalue after removing node {i}: {E_i[1]}")
+    # nx.draw_networkx(g_i)
+    # plt.show()
+    A.append(E_i[1])
+
+fig, ax = plt.subplots()            
+ax.plot(range(1,d*n+2,n), A[1:])  
+plt.show() 
